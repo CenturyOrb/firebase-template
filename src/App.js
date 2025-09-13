1c1afa090868565ceaba53ba561b0192974f5cc8
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-09-13 15:57:39 -0400

    Finished email and password auth method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+/* 
+	Useful links:
+		- https://firebase.google.com/docs/auth/web/password-auth?authuser=0#web
+		- https://firebase.google.com/docs/auth/web/start?authuser=0
+*/
 
+import './App.css'
+import { useState, useRef, useEffect } from 'react'
+import FirebaseWebApp from './config/firebase-config.js'
+import { getAuth, 
+		createUserWithEmailAndPassword,
+		signInWithEmailAndPassword,
+		signOut,
+		onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth(FirebaseWebApp);
+	
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [appUser, setAppUser] = useState(null);
+	const [email, setEmail] = useState('');	
+	const [password, setPassword] = useState('');
+	const createAccountButton = useRef(null);
+	const signInButton = useRef(null);
+	const signOutButton = useRef(null);
+
+	// starts the auth state listener when component first mounts
+	// when component unmounts turns off listener
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (user) => { // runs this callback each time state changes
+			setAppUser(user);
+		})
+		
+   		return unsubscribe;
+	}, []);
+
+	useEffect(() => {
+		console.log(appUser);
+	}, [appUser]);
+
+	const handleCreateAccount = async () => {
+		// implement functionality to create account with firebase auth
+		// remember to set password policy in firebase console
+
+		// (1) make a validatePassword() function in firebase-utils.js etc that
+		// validates password on account creation (should be === to password policy)
+		// didn't do that in this example
+		if (!email || !password) return;
+	
+		// (2) create new user with email and password given 
+		try {
+			createUserWithEmailAndPassword(auth, email, password);
+			console.log('Created a new account lets goo');
+		} catch (error) { console.error(error.message) }
+		finally {
+			setEmail('');
+			setPassword('');
+		}
+	}
+
+	const handleSignIn = async () => {
+    	// implement functionality to sign in with firebase auth
+    	try {
+    		await signInWithEmailAndPassword(auth, email, password);
+			console.log('Signed in yayayayay!');
+    	} catch (error) { console.error(error.message) }
+    	finally {
+    		setEmail('');
+    		setPassword('');
+    	}
+    }
+
+	const handleSignOut = async () => {
+		// implement functionality to sign out with firebase auth
+		try {
+			await signOut(auth); // signs out unconditionally
+			console.log('Signed out lets go!');
+		} catch (error) { console.error(error) }
+	}
+
+  	return (
+  	  <div className="App">
+  	  	<input 
+			type='email'
+			value={email} 
+			placeholder='Email...' 
+			onChange={ (event) => setEmail(event.target.value) }
+		/>
+  	  	<input 
+			type='password'
+			value={password} 
+			placeholder='Password...' 
+			onChange={ (event) => setPassword(event.target.value) }
+		/>
+		<button ref={createAccountButton} onClick={handleCreateAccount}>Create Account</button>
+		<button ref={signInButton} onClick={handleSignIn}>Sign In</button>
+		<button ref={signOutButton} onClick={handleSignOut}>Sign Out</button>
+  	  </div>
+  	);
 }
 
 export default App;
---
+++ src/config/firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUm-jJHMZ0VhAcgUXuOl7Q6feWPqcR54U",
+  authDomain: "fir-course-ca74e.firebaseapp.com",
+  projectId: "fir-course-ca74e",
+  storageBucket: "fir-course-ca74e.firebasestorage.app",
+  messagingSenderId: "243726200688",
+  appId: "1:243726200688:web:8fc45a0f49c37aca19e9b5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
